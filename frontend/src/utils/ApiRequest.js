ab47455829a0e543464b6ef65cb80b7dad4530c4
Rutuja Gaikwad <rutuja1308gaikwad@gmail.com>
2026-03-21 15:48:00 +0530

    fix axios + backend url

--- frontend/src/utils/ApiRequest.js
+++ frontend/src/utils/ApiRequest.js
@@ -1,4 +1,15 @@
+import axios from "axios";
+
 const host = process.env.REACT_APP_BACKEND_URL;
 
 export const registerAPI = `${host}/api/auth/register`;
 export const loginAPI = `${host}/api/auth/login`;
+
+export const addTransaction = async (data) => {
+  try {
+    const res = await axios.post(`${host}/api/transactions`, data);
+    return res.data;
+  } catch (err) {
+    console.error(err);
+  }
+};
